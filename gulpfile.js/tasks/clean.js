64e6a1d39d5321bb764137277bfb23d5d67ee888
TohaRodiv <toha.rodiv@yandex.ru>
2021-02-20 20:43:02 +0300

    Refactor

--- gulpfile.js/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-const del = require("del");
-const { dist_dir } = require("./../config");
-
-
-module.exports = function clean (cb) {
-	return del (dist_dir).then(() => {
-		cb ();
-	})
-};
---
+++ gulpfile.js/tasks/convertToJpg.js
@@ -0,0 +1,16 @@
+const {} = require ("./../config");
+
+
+module.exports = () => {
+	return src(path.src.img)
+		.pipe(gm(gmfile => {
+			return gmfile.compress("JPEG");
+		}))
+		.pipe(gm(gmfile => {
+			return gmfile.setFormat("jpeg");
+		}))
+		.pipe(gm(gmfile => {
+			return gmfile.quality(70); // todo: change quality
+		}))
+		.pipe(dest(path.dist.img));
+}
